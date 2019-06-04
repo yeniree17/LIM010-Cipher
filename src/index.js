9a3217e7e69cda1231b6df56f90a0327486730a9
yeniree17 <yeniree17@gmail.com>
2019-06-03 22:36:38 -0500

    avance2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 /* Acá va tu código */
-const botonNombre = document.getElementById.('botonNombre')
-botonNombre.addEventListener ('click', () => {}) 
+const nombreUsuario= document.getElementById('nombre');
+
+const botonNombre = document.getElementById('botonNombre');
+
+/* botonNombre.addEventListener ('click', mostrarNombre ());
+/*=> {
+const nombre = nombreUsuario.value;
+});*/
+
+/* botonPassword.addEventListener ('click', () => {
+    const password = password.value;
+    }); */
+
+function mostrarNombre () {
+    const nombre= nombreUsuario.value;
+    document.getElementById('nombre2').innerHTML='Hola '+ nombre + ' !';
+    document.getElementById('inicio').style.display="none"; //Oculta la seccion de inicio
+    document.getElementById('opciones').style.display="block"; //Muestra la seccion de seleccionar metodo
+}
+
