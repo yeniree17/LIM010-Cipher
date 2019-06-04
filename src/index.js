5a39beb261b2559f6589c366e542c141623889c0
yeniree17 <yeniree17@gmail.com>
2019-06-04 13:10:34 -0500

    avance 3 arquitectura HTML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 /* Acá va tu código */
 const nombreUsuario= document.getElementById('nombre');
+const password = document.getElementById('password');
+const clave= "laboratoria".toUpperCase();
+const boton= document.getElementById("botonPassword");
+let c=1;
+boton.addEventListener("click", function(){
+const name= password.value;
+    if(password==clave){
+      alert('Bienvenido');
+    }
+    if else(c>2){
+        alert('ERROR Ya no puedes ingresar');
+    }
+    else{
+        alert ('Intenta de nuevo');
+        c++;
 
-const botonNombre = document.getElementById('botonNombre');
+    }
+})
+
+
+
 
 /* botonNombre.addEventListener ('click', mostrarNombre ());
 /*=> {
@@ -12,10 +31,10 @@ const nombre = nombreUsuario.value;
     const password = password.value;
     }); */
 
-function mostrarNombre () {
+/*function mostrarNombre () {
     const nombre= nombreUsuario.value;
     document.getElementById('nombre2').innerHTML='Hola '+ nombre + ' !';
     document.getElementById('inicio').style.display="none"; //Oculta la seccion de inicio
     document.getElementById('opciones').style.display="block"; //Muestra la seccion de seleccionar metodo
-}
+}*/
 
