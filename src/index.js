83ca988533bab175ca8cdd785c46c0b4bd72dc9d
yeniree17 <yeniree17@gmail.com>
2019-06-05 13:46:00 -0500

    modf de funcion acceder

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 /* Acá va tu código */
 const nombreUsuario= document.getElementById('nombre');
 const password = document.getElementById('password');
-const clave= "laboratoria".toUpperCase();
+const clave= 'laboratoria'.toUpperCase();
 const boton= document.getElementById("botonPassword");
-let c=1;
-boton.addEventListener("click", function(){
-const name= password.value;
-    if(password==clave){
-      alert('Bienvenido');
-    }
-    if else(c>2){
-        alert('ERROR Ya no puedes ingresar');
+let intento= 0;
+
+
+boton.addEventListener('click', function() {
+const nombre= nombreUsuario.value;
+const psw= password.value;
+
+    if(psw == clave){
+    
+    document.getElementById('inicio').style.display='none'; //oculto la seccion de inicio ventana1
+    document.getElementById('opciones').style.display='block'; //muestro la seccion de seleccionar metodo ventana2
+    /*document.getElementById('nombre2').innerHTML= "Hola "+ nombre + "!";*/
+}
+    else if(intento>1){
+        alert('Ya utilizaste todos tus intentos, en estos momentos no podrás ingresar!');
     }
     else{
-        alert ('Intenta de nuevo');
-        c++;
-
+        alert ('Contraseña Incorrecta! Recuerda colocar la clave en mayúscula, al 3er intento fallido, ya no podras ingresar');
+        intento++;
     }
 })
 
