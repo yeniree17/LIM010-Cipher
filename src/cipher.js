70fb9b9030a61393db19ffe729e5725040b2b537
yeniree17 <yeniree17@gmail.com>
2019-06-05 17:09:02 -0500

    actualizacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,13 @@
+/*const offset= document.getElementById*/
 window.cipher = {
   encode: (offset, string) => {
+
+    let= solved = ''; 
+    for(let:i=0; i<string.length; i++){
+
+      const asciiNum= string[i].charCodeAt();
+      
+    }
     /* Acá va tu código que cifra*/
   },
   decode: (offset, string) => {
