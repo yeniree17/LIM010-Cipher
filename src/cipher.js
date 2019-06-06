445882d0e9ffe6f14d563b496aa02d9c2b01a24f
yeniree17 <yeniree17@gmail.com>
2019-06-06 16:36:12 -0500

    visualizacion de ventanas

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,31 @@
-/*const offset= document.getElementById*/
+/* Acá va tu código que cifra*/
+const offset= parseInt(document.getElementById('claveCif').value);
+const string= document.getElementById('textCif').value;
 window.cipher = {
   encode: (offset, string) => {
+  let resulCif = '';
+    for(let i=0; i<string.length; i++)
+    let asciiNum= (i.charCodeAt(0)-65 + offset) % 26 + 65;
+      if (asciiNum>=65 && asciiNum<=90){
+        resulCif += string.fromCharCode(asciiNum)
+      } 
+    } return resulCif;
 
-    let= solved = ''; 
-    for(let:i=0; i<string.length; i++){
-
-      const asciiNum= string[i].charCodeAt();
-      
-    }
-    /* Acá va tu código que cifra*/
   },
+  
+  /*decode: (offset, string) => {
+    Acá va tu código que descifra}*/
+
+const offset= document.getElementById('claveDescif');
+const string= document.getElementById('')
+window.cipher = {
   decode: (offset, string) => {
-    /* Acá va tu código que descifra*/
-  }
-};
+  let resulDescif = '';
+    for(let i=0; i<string.length; i++)
+    let asciiNum= (i.charCodeAt(0)-65 + offset) % 26 - 65;
+      if (asciiNum>=65 && asciiNum<=90){
+        resulDescif += string.fromCharCode(asciiNum)
+      } 
+    } return resulDesCif;
+
+  };
--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 /* Acá va tu código */
-const nombreUsuario= document.getElementById('nombre');
+const nombreUsuario = document.getElementById('nombre');
 const password = document.getElementById('password');
-const clave= 'laboratoria'.toUpperCase();
-const boton= document.getElementById("botonPassword");
-let intento= 0;
+const clave = 1;
+/*'laboratoria'.toUpperCase();*/
+const boton = document.getElementById("botonPassword");
+let intento = 0;
 
-
-boton.addEventListener('click', function() {
-const nombre= nombreUsuario.value;
-const psw= password.value;
-
-    if(psw == clave){
-    
-    document.getElementById('inicio').style.display='none'; //oculto la seccion de inicio ventana1
-    document.getElementById('opciones').style.display='block'; //muestro la seccion de seleccionar metodo ventana2
-    /*document.getElementById('nombre2').innerHTML= "Hola "+ nombre + "!";*/
-}
-    else if(intento>1){
+boton.addEventListener('click', (event) => {
+    event.preventDefault()
+    const nombre = nombreUsuario.value;
+    const psw = password.value;
+    if (psw == clave) {
+        document.getElementById('inicio').style.display = 'none'; //oculto la seccion de inicio ventana1
+        document.getElementById('opciones').style.display = 'block'; //muestro la seccion de seleccionar metodo ventana2
+        document.getElementById('nombre2').innerHTML = "Hola " + nombre + "!";
+    }
+    else if (intento > 1) {
         alert('Ya utilizaste todos tus intentos, en estos momentos no podrás ingresar!');
     }
-    else{
-        alert ('Contraseña Incorrecta! Recuerda colocar la clave en mayúscula, al 3er intento fallido, ya no podras ingresar');
+    else {
+        alert('Contraseña Incorrecta! Recuerda colocar la clave en mayúscula, al 3er intento fallido, ya no podras ingresar');
         intento++;
     }
-})
-
-
+});
+const boton2 = document.getElementById('darConsejo');
+boton2.addEventListener('click', () => {
+    document.getElementById('opciones').style.display = 'none'; //oculto la seccion de opciones ventana2
+    document.getElementById('cifMje').style.display = 'block'; //muestro la seccion de cifrado
+});
+const boton3 = document.getElementById('descifrarSecreto');
+boton3.addEventListener('click', () => {
+    document.getElementById('opciones').style.display= 'none';
+    document.getElementById('descifMje').style.display= 'block';
+    document.getElementById('nombre3').innerHTML = "BienvenidX " + nombre + "!";
+});
 
 
 /* botonNombre.addEventListener ('click', mostrarNombre ());
